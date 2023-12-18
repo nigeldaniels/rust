cd387691b9bcbca292f26435798bc39c49c89466
nigeldaniels <nigel.daniels@gmail.com>
2023-12-17 17:41:23 -0800

    demonstrates functions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,6 +2,13 @@ fn main() {
     shaddow_example();
     tupple_example();
     array_example();
+    example_function_params(75, 88);
+
+    let x = example_function_with_simple_return();
+    println!("The value of returned by the function is: {}", x);
+
+    let y = plus_one(6);
+    println!("The value of y is: {}", y)
 }
 
 fn shaddow_example() {
@@ -38,4 +45,27 @@ fn array_example() {
     println!("one way first: {}, second: {}", first, second);
     println!("Another way third: {}, fourth: {}", test_array[3], test_array[4]);
 
+}
+
+fn example_function_params(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is {}", y);
+}
+
+fn example_statement_in_function(){
+    let x = 5;
+    let y = {
+        let x = 3;
+        x+1
+    };
+
+    println!("The Value of y is: {}", y );
+}
+
+fn example_function_with_simple_return () -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
