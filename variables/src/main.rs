cc306e38b260eb0278bb490adc65c8c9fb98d930
nigeldaniels <nigel.daniels@gmail.com>
2023-12-17 16:54:00 -0800

    demonstrates tuples and tuple indexs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,5 +3,18 @@ fn main() {
     let x = x + 1;
 
     let x = x * 2;
-    println!("The value of x is: {}", x)
+    println!("The value of x is: {}", x);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y , z) = tup;
+
+    println!("The value of y is: {}",y);
+    println!("The fallowing demonstrates acessing tuple by index");
+
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+
+    println!("the first element is {}, the second one is {}, the third one is {}", tup.0, tup.2, tup.2);
+    println!("you can also do five_hundred: {}, six_point_four {} and one: {}", five_hundred, six_point_four, one )
 }
