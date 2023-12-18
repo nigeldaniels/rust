aca736cd4b7730fac1e6004e7cea27f422296401
nigeldaniels <nigel.daniels@gmail.com>
2023-12-17 17:22:09 -0800

    demonstrates the use of functions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
+    shaddow_example();
+    tupple_example();
+    array_example();
+}
+
+fn shaddow_example() {
+    println!("shaddow example");
     let x = 5;
     let x = x + 1;
 
     let x = x * 2;
     println!("The value of x is: {}", x);
+}
 
+fn tupple_example() {
+    println!("tupple example");
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     let (x, y , z) = tup;
 
@@ -16,5 +26,16 @@ fn main() {
     let one = tup.2;
 
     println!("the first element is {}, the second one is {}, the third one is {}", tup.0, tup.2, tup.2);
-    println!("you can also do five_hundred: {}, six_point_four {} and one: {}", five_hundred, six_point_four, one )
+    println!("you can also do five_hundred: {}, six_point_four {} and one: {}", five_hundred, six_point_four, one );
+}
+
+fn array_example() {
+
+    let test_array = [1,2,3,4,5];
+    let first = test_array[0];
+    let second = test_array[1];
+
+    println!("one way first: {}, second: {}", first, second);
+    println!("Another way third: {}, fourth: {}", test_array[3], test_array[4]);
+
 }
